12e664db5433b25b1e3d8e547f8c4877b6fe5a06
Tyler Lum <tylergwlum@gmail.com>
2019-10-28 22:19:08 -0700

    Add basic structure for custom AIS ROS msg and simple Python publisher

---
+++ scripts/test_custom_talker.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import rospy
+from test_package.msg import AIS
+
+def talker():
+    pub = rospy.Publisher('custom_AIS', AIS)
+    rospy.init_node('custom_AIS', anonymous=True)
+    r = rospy.Rate(10) #10hz
+    msg = AIS()
+    msg.MMSI = 100000000
+    msg.heading = 40
+    msg.speedOverGround = 0.1
+    msg.latitude = 50.9
+    msg.longitude = -123.3
+    msg.lastSeen = rospy.Time(10)
+
+    while not rospy.is_shutdown():
+        rospy.loginfo(msg)
+        pub.publish(msg)
+        r.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException: pass
