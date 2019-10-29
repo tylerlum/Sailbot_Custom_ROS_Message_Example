c07d679fa8bcdc706b4c4894a6fc9b6d762d6f8d
Tyler Lum <tylergwlum@gmail.com>
2019-10-28 22:48:21 -0700

    Add publishing to subscriber

--- scripts/test_custom_listener.py
+++ scripts/test_custom_listener.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 import rospy
 from test_package.msg import AIS
+from geometry_msgs.msg import Pose2D
+
+msg = Pose2D()
+msg.x = 0
+msg.y = 0
+msg.theta = 0
 
 def callback(data):
     rospy.loginfo(data)
+    msg.x = data.latitude
+    msg.y = data.longitude
 
 def listener():
     rospy.init_node('custom_AIS_listener', anonymous=True)
     rospy.Subscriber("custom_AIS", AIS, callback)
+    pub = rospy.Publisher("path", Pose2D)
+    r = rospy.Rate(10) #10hz
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+    while not rospy.is_shutdown():
+        pub.publish(msg)
+        r.sleep()
 
 if __name__ == '__main__':
     listener()
